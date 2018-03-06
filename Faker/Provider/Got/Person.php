5e4cec9bf0a6015d0e6c47681e84c50ea0a35857
Lewis Voncken <lewis@experius.nl>
2018-03-06 15:28:55 +0100

    [TASK] Added new Firstnames and Surnames to the Got Provider

--- Faker/Provider/Got/Person.php
+++ Faker/Provider/Got/Person.php
@@ -7,18 +7,36 @@ class Person extends \Faker\Provider\en_US\Person {
 
     protected static $firstNameMale = array(
         'John','Gregor','Tyrion','Joffrey','Sandor','Ramsay','Eddard','Hodor','Petyr','Bran','Robb','Daario','Theon',
-        'Tommon','High','Stannis','Jamie','Tormund','Oberyn','Renling','Podrick','Davos','Khal'
+        'Tommon','High','Stannis','Jamie','Tormund','Oberyn','Renling','Podrick','Davos','Khal', 'Bronn', 'Benjen',
+	'Roose', 'Kevan', 'Euron', 'Bryden', 'Jojen', 'Jorah', 'Samwell', 'Sam', 'Rickon', 'Loras', 'Edmure', 'Tywin',
+	'Gendry', 'Jaime', 'Mace', 'Trystane', 'Olly', 'Jaqen', 'Varys', 'Renly', 'Jeor', 'Tommen', 'Quentyn', 'Jon',
+	'Mance', 'Viserys', 'Lancel'
     );
 
     protected static $firstNameFemale = array(
         'Kalisi','Arya','Sansa','Cersei','Shae','Margaery','Melisandre','Ygritte','Brienne','Catelyn','Missandei',
-        'Meera','Myrcella'
+        'Meera','Myrcella', 'Shireen', 'Lyanna', 'Yara', 'Daenerys', 'Gilly', 'Asha', 'Mordane', 'Myranda', 'Elia',
+	'Talisa', 'Ellaria', 'Jeyne', 'Arianne', 'Olenna', 'Roslin', 'Osha', 'Walda', 'Selyse', 'Ros', 'Lysa', 'Penny',
+	'Obara', 'Unella', 'Tyene', 'Nymeria', 'Khaleesi'
     );
 
     protected static $lastName = array(
-        'Snow','Targaryen','Stark','Clegane','Lannister','Baratheon','Tyrell','Clegane','Bolton','of Tarth','Stark',
-        'Naharis','Greyjoy','Baratheon','Septon','Giantsbane','Martell','Baratheon','Payne','Seaworth','Drogo','Baelish',
-        'Sparrow'
+        'Snow','Targaryen','Stark', 'Clegane', 'Lannister', 'Baratheon', 'Tyrell', 'Bolton','of Tarth','Stark',
+        'Naharis', 'Greyjoy', 'Baratheon', 'Septon', 'Giantsbane', 'Martell', 'Baratheon', 'Payne', 'Seaworth', 'Drogo', 'Baelish',
+        'Sparrow', 'Hodor', 'Tully', 'of Winterfell', 'Frey', 'Mormont', 'of Westeros', 'White Walker', 'Tarly', 'Maegyr',
+	'Sand', 'Reed', 'Reyne', 'Rosby', 'Forrester', 'Arryn', 'Ashford', 'Baratheon of Kings Landing', 'Beesburry',
+	'Belgrave', 'Blackfyre', 'Blackmont', 'Blacktyde', 'Blackwood', 'Blount', 'Botley', 'Bracken', 'Branfield', 'Bulwer',
+	'Cargyll', 'Caron', 'Cassel', 'Casterly', 'Caulfield', 'Cerwyn', 'Connington', 'Corbray', 'Crakehall',
+	'from the Crownlands', 'Cuy', 'Dalt', 'Darry', 'Dayne', 'Dondarrion', 'Dormund', 'from Dorne', 'Durrandon', 'Dustin',
+	'Egen', 'Erenford', 'Estermont', 'Florent', 'Fossoway', 'Glover', 'Greenfield', 'Harlaw', 'Harroway', 'Hightower',
+	'Hoare', 'Hollard', 'Holt', 'Hornwoord', 'Hunter', 'from the Iron Islands', 'Jordayne', 'Justman', 'Karstark',
+	'Kenning', 'Lannister of Lannisport', 'Lefford', 'of Loraq', 'Lorch', 'Lothston', 'Lynderly', 'Mallister', 'Manwoody',
+	'Marbrand', 'Marsh', 'Mollen', 'Moore', 'Mooton', 'Morrigen', 'Mudd', 'from the Narrow Sea', 'from the North',
+	'Oakheart', 'Poole', 'of Qaggaz', 'Qoherys', 'Qorgyle', 'from the Reach', 'Redwyne', 'from the Riverlands', 'Royce',
+	'Rykker', 'Ryswell', 'Santagar', 'Sarsfield', 'Selmy', 'Serrett', 'Slynt', 'Smallwood', 'Staunton', 'Stokeworth',
+	'from the Stormlands', 'Strong', 'Swann', 'Swyft', 'Tallhart', 'Thorne', 'Tollett', 'Towers', 'Toyne', 'Trant',
+	'Uller', 'Umber', 'from the Vale', 'Velaryon', 'Waynwood', 'Wells', 'from the Westerlands', 'Westerling', 'Whent',
+	'Whitehill', 'Wode', 'Wylde', 'Yarwyck'
     );
 
 }
